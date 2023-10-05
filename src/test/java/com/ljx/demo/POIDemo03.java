8a73197041c2331b8e4df70c296a1e1bf400b32f
李君祥 <2952434583@qq.com>
2023-10-05 20:23:09 +0800

    百万数据导入

---
+++ src/test/java/com/ljx/demo/POIDemo03.java
@@ -0,0 +1,7 @@
+package com.ljx.demo;
+
+public class POIDemo03 {
+    public static void main(String[] args) throws Exception{
+        new ExcelParser().parse("E:\\学习\\Java\\编程强化\\Java报表数据可视化过程\\day02\\资料\\百万数据.xlsx");
+    }
+}
